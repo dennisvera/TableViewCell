9f93c5a41927313c9badaaa26ae4292b395544bc
Dennis Vera <dennis.vera@wework.com>
2020-04-06 18:21:42 -0400

    configures UITextView and handles dynamic types

--- Auteurs/ViewControllers/AuteurDetailViewController.swift
+++ Auteurs/ViewControllers/AuteurDetailViewController.swift
@@ -37,6 +37,9 @@ class AuteurDetailViewController: UIViewController {
     super.viewDidLoad()
     title = selectedAuteur.name
     self.tableView.contentInsetAdjustmentBehavior = .never
+
+    tableView.rowHeight = UITableView.automaticDimension
+    tableView.estimatedRowHeight = 300
   }
 }
 
@@ -46,11 +49,41 @@ extension AuteurDetailViewController: UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FilmTableViewCell
     let film = selectedAuteur.films[indexPath.row]
-    cell.textLabel?.text = film.plot
+
+    cell.selectionStyle = .none
+
+    cell.filmImageView.image = UIImage(named: film.poster)
+    
+    cell.filmTitleLabel.text = film.title
+    cell.filmTitleLabel.textColor = .white
+    cell.filmTitleLabel.textAlignment = .center
+
+    cell.moreInfoTextView.text = film.isExpanded ? film.plot : moreInfoText
+    cell.moreInfoTextView.textAlignment = film.isExpanded ? .left : .center
+    cell.moreInfoTextView.textColor = film.isExpanded ? UIColor(displayP3Red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0) : .red
     
     return cell
   }
 }
 
+extension AuteurDetailViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    guard let cell = tableView.cellForRow(at: indexPath) as? FilmTableViewCell else { return }
+
+    var film = selectedAuteur.films[indexPath.row]
+
+    film.isExpanded = !film.isExpanded
+    selectedAuteur.films[indexPath.row] = film
+
+    cell.moreInfoTextView.text = film.isExpanded ? film.plot : moreInfoText
+    cell.moreInfoTextView.textAlignment = film.isExpanded ? .left : .center
+    cell.moreInfoTextView.textColor = film.isExpanded ? UIColor(displayP3Red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0) : .red
+
+    tableView.beginUpdates()
+    tableView.endUpdates()
+
+    tableView.scrollToRow(at: indexPath, at: .top, animated: true)
+  }
+}
