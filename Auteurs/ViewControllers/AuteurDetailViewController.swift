fe1051dcb4a78f3c122f42c056841f40a937cff7
DennisVera <dennisvera.z@gmail.com>
2020-08-11 18:03:48 -0400

    Create AuteurDetailTableViewCell

--- Auteurs/ViewControllers/AuteurDetailViewController.swift
+++ Auteurs/ViewControllers/AuteurDetailViewController.swift
@@ -29,14 +29,21 @@
 import UIKit
 
 class AuteurDetailViewController: UIViewController {
+  
+  @IBOutlet weak var tableView: UITableView!
+  
   var selectedAuteur: Auteur!
   let moreInfoText = "Tap For Details >"
-  @IBOutlet weak var tableView: UITableView!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     title = selectedAuteur.name
     self.tableView.contentInsetAdjustmentBehavior = .never
+    
+    // Let the table view estimate the row height.
+    // The estimated row height is still needed to be declared in order for the tableview to know what size to start with.
+    tableView.rowHeight = UITableView.automaticDimension
+    tableView.estimatedRowHeight = 300
   }
 }
 
@@ -46,11 +53,51 @@ extension AuteurDetailViewController: UITableViewDataSource {
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell",
+                                                   for: indexPath) as? AuteurDetailTableViewCell else {
+                                                    fatalError("Unable to Dequeue Cell.")
+    }
+    
     let film = selectedAuteur.films[indexPath.row]
-    cell.textLabel?.text = film.plot
+    
+    cell.filmImageView.image = UIImage(named: film.poster)
+    
+    cell.filmTitleLabel.text = film.title
+    cell.filmTitleLabel.textColor = .white
+    cell.filmTitleLabel.textAlignment = .center
+    
+    cell.moreInfotextView.text = film.isExpanded ? film.plot : moreInfoText
+    cell.moreInfotextView.textAlignment = film.isExpanded ? .left : .center
+    cell.moreInfotextView.textColor = film.isExpanded ? UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0) : .red
+    
+    cell.selectionStyle = .none
     
     return cell
+  }
+}
+
+extension AuteurDetailViewController: UITableViewDelegate {
+  
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+    // 1
+    guard let cell = tableView.cellForRow(at: indexPath) as? AuteurDetailTableViewCell else { return }
+    
+    // 2
+    var film = selectedAuteur.films[indexPath.row]
+    film.isExpanded = !film.isExpanded
+    
+    // 3
+    cell.moreInfotextView.text = film.isExpanded ? film.plot : moreInfoText
+    cell.moreInfotextView.textAlignment = film.isExpanded ? .left : .center
+    cell.moreInfotextView.textColor = film.isExpanded ? UIColor(red: 0.75, green: 0.75, blue: 0.75, alpha: 1.0) : .red
+    
+    // 4
+    tableView.beginUpdates()
+    tableView.endUpdates()
+    
+    // 5
+    tableView.scrollToRow(at: indexPath, at: .top, animated: true)
   }
 }
 
