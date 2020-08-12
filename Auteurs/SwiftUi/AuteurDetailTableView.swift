3901dcecc64ffbc0b4eb76d5a1b71e88ae5891c2
DennisVera <dennisvera.z@gmail.com>
2020-08-11 20:26:41 -0400

    Create the Auteur view and Detail view with SwiftUI

--- Auteurs/SwiftUi/AuteurDetailTableView.swift
+++ Auteurs/SwiftUi/AuteurDetailTableView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,23 +30,59 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
 import SwiftUI
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
+struct AuteurDetailTableView: View {
   
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    
-    // Use a UIHostingController as window root view controller
-    if let windowScene = scene as? UIWindowScene {
-      let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UIHostingController(rootView: AuteurTableView(auteurs: Auteur.auteursFromBundle()))
-      self.window = window
-      window.makeKeyAndVisible()
+  let selectedAuteur: Auteur!
+  
+  var body: some View {
+    List {
+      ForEach(selectedAuteur.films, id: \.title) { film in
+        FilmRow(film: film)
+          .listRowBackground(Color(red: 22.0/255.0, green: 22.0/255.0, blue: 22.0/255.0))
+      }
     }
+  }
+}
+
+struct FilmRow: View {
+  
+  let film: Film
+  @State var isExpanded = false
+  @State var moreInfo = "Tap For Details >"
+  
+  let moreInfoColor = Color.red
+  let greyColor = Color(red: 0.75, green: 0.75, blue: 0.75, opacity: 1.0)
+  
+  var body: some View {
+    
+    VStack(alignment: .center) {
+      Image(film.poster)
+        .resizable()
+        .padding(8)
+        .aspectRatio(contentMode: .fit)
+      Text(film.title)
+        .padding(8)
+        .foregroundColor(.white)
+        .font(.system(size: 17))
+      Text(moreInfo)
+        .padding(8)
+        .font(.system(size: 16))
+        .foregroundColor(self.isExpanded ? greyColor : moreInfoColor)
+    }.gesture(
+      TapGesture()
+        .onEnded { _ in
+          (self.isExpanded == false) ? (self.moreInfo = self.film.plot) : (self.moreInfo = "Tap For Details >")
+          self.isExpanded.toggle()
+      }
+    )
+  }
+}
+
+struct AuteurDetailTableView_Previews: PreviewProvider {
+  
+  static var previews: some View {
+    AuteurDetailTableView(selectedAuteur: Auteur.auteursFromBundle().first!)
   }
 }
--- Auteurs/SwiftUi/AuteurTableView.swift
+++ Auteurs/SwiftUi/AuteurTableView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-///
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,23 +30,62 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import UIKit
 import SwiftUI
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-  var window: UIWindow?
+struct AuteurTableView: View {
   
-  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    
-    // Use a UIHostingController as window root view controller
-    if let windowScene = scene as? UIWindowScene {
-      let window = UIWindow(windowScene: windowScene)
-      window.rootViewController = UIHostingController(rootView: AuteurTableView(auteurs: Auteur.auteursFromBundle()))
-      self.window = window
-      window.makeKeyAndVisible()
+  let auteurs: [Auteur]
+  
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(auteurs, id: \.name) { auteur in
+          NavigationLink(destination: AuteurDetailTableView(selectedAuteur: auteur)) {
+            AuteurRow(autuer: auteur)
+          }
+            .listRowBackground(Color(red: 22.0/255.0, green: 22.0/255.0, blue: 22.0/255.0))
+        }
+      }.navigationBarTitle("Auteurs")
     }
+  }
+}
+
+struct AuteurRow: View {
+  
+  let autuer: Auteur
+  
+  var body: some View {
+    VStack {
+      VStack (alignment: .center) {
+        Image(autuer.image)
+          .cornerRadius(200)
+          .padding(.bottom, 8)
+        Text(autuer.name)
+          .padding(.top, 8)
+          .padding(.bottom, 8)
+          .padding(.leading, 8)
+          .font(.system(size: 18))
+          .foregroundColor(.white)
+      }
+      VStack (alignment: .leading) {
+        Text(autuer.bio)
+          .padding(.top, 8)
+          .font(.system(size: 17))
+          .foregroundColor(Color(red: 0.75, green: 0.75, blue: 0.75))
+        Text(autuer.source)
+          .italic()
+          .padding(.top, 8)
+          .padding(.bottom, 16)
+          .font(.system(size: 13))
+          .alignmentGuide(.leading, computeValue: { _ in -8})
+          .foregroundColor(Color(red: 0.74, green: 0.74, blue: 0.74))
+      }
+    }
+  }
+}
+
+struct AuteurTableView_Previews: PreviewProvider {
+  static var previews: some View {
+    AuteurTableView(auteurs: Auteur.auteursFromBundle())
   }
 }
